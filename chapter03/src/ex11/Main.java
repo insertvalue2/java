491b589529c7bff3b281ac858f4fa74832250dcb
DESKTOP-VLHSV2N\lalad <insertvalue@naver.com>
2021-07-09 10:55:08 +0900

    swing1

---
+++ chapter03/src/ex11/Main.java
@@ -0,0 +1,27 @@
+package ex11;
+
+import javax.swing.JFrame;
+
+class MyFrame extends JFrame {
+
+	public MyFrame() {
+
+		setTitle("CardLayout");
+
+		setSize(400, 200);
+
+		setVisible(true);
+
+	}
+
+}
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		MyFrame f = new MyFrame();
+
+	}
+
+}
