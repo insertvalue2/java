8299e47ca4d57928a4ea27e5969a2cc6b0f3ae2c
DESKTOP-VLHSV2N\lalad <insertvalue@naver.com>
2021-07-16 11:08:54 +0900

    Runable 문법 메인쓰레드 동작

---
+++ chapter04/src/ch01/RunnableTest2.java
@@ -0,0 +1,35 @@
+package ch01;
+
+import javax.swing.JFrame;
+
+class MyRunable extends JFrame {
+	
+	int grade = 10; 
+	
+	public MyRunable() {
+		runable.run();
+	}
+	
+	Runnable runable = new Runnable() {
+		@Override
+		public void run() {
+			for (int i = 0; i < 10; i++) {
+				System.out.print("--");
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	};
+}
+
+public class RunnableTest2 {
+
+	public static void main(String[] args) {
+		MyRunable myRunable = new MyRunable();
+	}
+
+}
--- chapter04/src/ch05/CallBackTest.java
+++ chapter04/src/ch05/CallBackTest.java
@@ -12,6 +12,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+// 약속 (규약) 
 interface MyCallBack {
 	public void add();
 
